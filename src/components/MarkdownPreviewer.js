3b00cb20060a055bc1f5136e683f7c2c45c0953a
Hieu Lai <hieuqlai@gmail.com>
2018-04-30 15:14:28 +1000

    added styles

--- src/components/MarkdownPreviewer.js
+++ src/components/MarkdownPreviewer.js
@@ -13,13 +13,18 @@ export default class MarkdownPreviewer extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>   
-        <textarea 
-          type="text" 
-          onChange={this.handleChange}
-          value={this.state.input}
-        ></textarea>
-        <div dangerouslySetInnerHTML={{ __html: marked(this.state.input)}} />    
+        <h1 className="header">Markdown Previewer</h1>   
+        <div className="container">
+          <textarea 
+            className="textArea"
+            type="text" 
+            onChange={this.handleChange}
+            value={this.state.input}
+          ></textarea>
+          <div
+            className="outputArea" 
+            dangerouslySetInnerHTML={{ __html: marked(this.state.input)}} />    
+        </div>
       </div>
     )
   }
