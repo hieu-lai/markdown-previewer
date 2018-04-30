179789ad615504f434b2f56f23fe16dc124aee57
Hieu Lai <hieuqlai@gmail.com>
2018-04-30 18:12:23 +1000

    added initial content

--- src/components/MarkdownPreviewer.js
+++ src/components/MarkdownPreviewer.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import marked from 'marked';
 
+const initialText = 'Heading\n=======\n\nSub-heading\n-----------\n\n' +
+                    '### Another deeper heading \n\nParagraphs are' +
+                    'separated \nby a blank line.\n\nLeave 2 spaces ' +
+                    'at the end of a line to do a  \nline break. \n\n' +
+                    'Text attributes *italic*, **bold**, `monospace`, ' +
+                    '\n~~strikethrough~~ .\n\nShopping list:\n\n ' +
+                    '*apples\n *oranges\n *pears\n\nNumbered list:\n\n ' +
+                    '1.apples\n 2.oranges\n 3.pears\n\n' + 
+                    "No worries ---mate---she'll be right."; 
+
 export default class MarkdownPreviewer extends React.Component {
   state = {
-    input: ''
+    input: initialText
   };
   handleChange = (event) => {
-    const change = event.target.value; 
-    //console.log(change);
+    const change = event.target.value;
     this.setState((prevState) => ({ input: change }));  
   };
   render() {
